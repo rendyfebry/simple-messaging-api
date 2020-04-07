fc18cbd191831740df68edaebb3575dec1f03bd1
Rendy Febry <rendyfebry@gmail.com>
2020-04-07 23:04:06 +0700

    Basic http server

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	routes := mux.NewRouter()
+	// Routes here
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("%s:%v", "0.0.0.0", "8080"),
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      routes,
+	}
+
+	fmt.Println(fmt.Sprintf("Environment: %s", "development"))
+	fmt.Println(fmt.Sprintf("Application URL: http://%s:%v", "localhost", "8080"))
+
+	// Run server in goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// Graceful shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("shutting down")
+	os.Exit(0)
+}
